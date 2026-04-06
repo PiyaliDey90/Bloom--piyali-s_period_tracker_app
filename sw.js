c05571118a0558c2afd614619cbfc9b4d844a455
piyal <learn.piyali30@gmail.com>
2026-04-05 23:32:16 -0400

    Initial commit: Bloom period tracker PWA

---
+++ sw.js
@@ -0,0 +1,59 @@
+const CACHE = 'bloom-v1'
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  './icons/apple-touch-icon.png',
+]
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS.filter(a => !a.includes('.png')))).catch(() => {})
+  )
+  self.skipWaiting()
+})
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  )
+  self.clients.claim()
+})
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request).then(res => {
+      if (res.ok && e.request.method === 'GET') {
+        const clone = res.clone()
+        caches.open(CACHE).then(c => c.put(e.request, clone))
+      }
+      return res
+    })).catch(() => caches.match('./index.html'))
+  )
+})
+
+self.addEventListener('push', e => {
+  const data = e.data?.json() ?? {}
+  e.waitUntil(
+    self.registration.showNotification(data.title ?? 'Bloom 🌸', {
+      body: data.body ?? 'Your period may be starting soon.',
+      icon: './icons/icon-192.png',
+      badge: './icons/icon-192.png',
+      tag: 'period-reminder',
+    })
+  )
+})
+
+self.addEventListener('notificationclick', e => {
+  e.notification.close()
+  e.waitUntil(
+    clients.matchAll({ type: 'window', includeUncontrolled: true }).then(list => {
+      const w = list.find(c => 'focus' in c)
+      return w ? w.focus() : clients.openWindow('./')
+    })
+  )
+})
